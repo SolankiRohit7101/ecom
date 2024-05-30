c852dec64b829ae59fe697ed1205645451bd7019
rohit <136067379+SolankiRohit7101@users.noreply.github.com>
2024-05-30 10:50:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "process.env.FRONTEND_URL", // specify the allowed origin
+    origin: "*", // specify the allowed origin
     credentials: true,
   })
 );
