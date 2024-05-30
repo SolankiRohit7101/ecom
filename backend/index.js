eb9060389789406dfe064bfd9e70c76771d531e5
rohit <136067379+SolankiRohit7101@users.noreply.github.com>
2024-05-30 11:44:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 const allowedOrigins = "*"
 
 const corsOptions = {
